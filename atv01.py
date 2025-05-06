0789ff5b1c4a22434f71b67913a65ee1309bb363
agathahelena3 <miauqsor@gmail.com>
2025-05-06 08:26:58 -0300

    aducuinei as atividades de 1 a 6

---
+++ atv01.py
@@ -0,0 +1,4 @@
+nome=["", "", "", "", " "]
+for x in range(len(nome)):
+    nome[x] = input("Digite o número de 5 alunos: ")
+print(nome)
---
+++ atv06.py
@@ -0,0 +1,5 @@
+num=[" ", " ", " ", " ", " "]
+for x in range(len(num)):
+    num[x]=input("Digite um número: ")
+for i in range( 4,-1,-1):
+    print(num[i])
